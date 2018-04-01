63fd0784cf1a1e53d0dc6c9f6e3c72ad2b709de8
vajsp <442026460@qq.com>
2018-04-01 09:16:55 +0800

    修改部署端口号

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.get('*', function (req, res) {
     const html = fs.readFileSync(path.resolve(__dirname, './public/index.html'), 'utf-8')
     res.send(html)
 })
-app.listen(3000,() => {
-    console.log('server at 3000')
+app.listen(3003,() => {
+    console.log('server at 3003')
 });
 
